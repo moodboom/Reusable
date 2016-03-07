3ab0544ee28e9ceeb7487cc882bee225c95c6438
Michael Behrns-Miller [aws-pa-mdm] <m@thedigitalmachine.com>
2016-03-07 14:49:48 -0500

    need multiple ways of seeing svn revision

--- JavaScript/node/reusable/get-svn-last-changed-rev.js
+++ JavaScript/node/reusable/get-svn-last-changed-rev.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 
 var shared = require('./shared.js');
-console.log(shared.svn_rev());
+console.log(shared.svn_last_changed_rev());
--- JavaScript/node/reusable/shared.js
+++ JavaScript/node/reusable/shared.js
@@ -90,8 +90,8 @@ exports.justLogResponse = function (error, stdout, stderr) {
 }    
 
 
-// =========== last_changed_rev: gets the SVN "last changed rev" for the current folder, as a string ============
-exports.last_changed_rev = function () {
+// =========== svn_last_changed_rev: gets the SVN "last changed rev" for the current folder, as a string ============
+exports.svn_last_changed_rev = function () {
 
     var run = exports.run_command_sync;
 
@@ -99,6 +99,22 @@ exports.last_changed_rev = function () {
 
     // extract the "Last Changed Rev"
     var regx = /^Last Changed Rev: (.*)$/gm;
+    var array_result = regx.exec(svn_info);
+
+    // return the first group result ([0] contains the whole result)
+    return array_result[1];
+}
+
+
+// =========== svn_rev: gets the SVN current revision for the current repo, as a string ============
+exports.svn_rev = function () {
+
+    var run = exports.run_command_sync;
+
+    var svn_info = run("svn", ["info"]);
+
+    // extract the "Last Changed Rev"
+    var regx = /^Revision: (.*)$/gm;
     var array_result = regx.exec(svn_info);
 
     // return the first group result ([0] contains the whole result)
