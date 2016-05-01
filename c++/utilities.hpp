790433a085b8ab34cbe3c1aeeb95c35219d2bc16
moodboom [ubuntu-melange] <m@thedigitalmachine.com>
2016-04-30 20:21:06 -0400

    comment

--- c++/utilities.hpp
+++ c++/utilities.hpp
@@ -107,7 +107,13 @@ static bool b_string_ends_in(const string& source, const string& search)
 //      0.*
 //
 // So for example, this will extract [0,58,1] from [git describe] versions like 0.58-1-gacd6168.
-void VersionParse(int result[4], const std::string& input)
+//
+// Following the precise definitions at semver.org,
+// versions are only and always compatible if their MAJOR versions match.
+// My versioning guidelines dictate that MINOR version differences mean
+// a db upgrade can and should be performed.
+// See http://bitpost.com/news/?p=1989.
+void VersionParse(int64_t result[4], const std::string& input)
 {
     std::istringstream parser(input);
     for(int idx = 0; idx < 4; idx++)
@@ -117,12 +123,33 @@ void VersionParse(int result[4], const std::string& input)
         if (parser.eof()) { for (int idxz = idx; idxz < 4; idxz++) result[idxz] = 0; return; }  // Quit if we hit the end.
     }
 }
-bool LessThanVersion(const std::string& a,const std::string& b)
+bool bVersionLessThan(const std::string& a,const std::string& b)
 {
-    int parsedA[4], parsedB[4];
+    int64_t parsedA[4], parsedB[4];
     VersionParse(parsedA, a);
     VersionParse(parsedB, b);
     return std::lexicographical_compare(parsedA, parsedA + 4, parsedB, parsedB + 4);
+}
+bool bVersionsAreCompatible(const std::string& a,const std::string& b)
+{
+    int64_t parsedA[4], parsedB[4];
+    VersionParse(parsedA, a);
+    VersionParse(parsedB, b);
+
+    return (parsedA[0] == parsedB[0]);  // same MAJOR
+}
+bool bVersionNeedsUpgrade(const std::string& vDB,const std::string& vApp)
+{
+    // The caller should ensure that the db version is less than or equal to the app version before getting here.
+    assert(!bVersionLessThan(vApp,vDB));
+
+    int64_t parsedDB[4], parsedApp[4];
+    VersionParse(parsedDB, vDB);
+    VersionParse(parsedApp, vApp);
+    return (
+            parsedDB[0] == parsedApp[0]     // same MAJOR
+        &&  parsedDB[1] < parsedApp[0]      // lower MINOR
+    );
 }
 //=========================================================
 
