ae7af700737df70efa8fe0801b69658816e59fee
Michael Behrns-Miller [case-ubuntu] <m@bitpost.com>
2017-07-31 23:11:52 -0400

    Account automation anyone

--- c++/http/HttpsClient.hpp
+++ c++/http/HttpsClient.hpp
@@ -7,6 +7,7 @@ using namespace std;
 
 
 // We derive from SimpleWeb::HttpsClient to add our base functionality.
+
 class HttpsClient : public Client<HTTPS>
 {
     typedef Client<HTTPS> inherited;
@@ -52,4 +53,56 @@ public:
 // Results extraction helpers
 static string get_body(std::shared_ptr<Client<HTTPS>::Response> response)           { if (response) return string(istreambuf_iterator<char>(response->content), {}); return ""; }
 static int32_t get_status_code(std::shared_ptr<Client<HTTPS>::Response> response)   { if (response) return boost::lexical_cast<int32_t>(response->status_code.substr(0,3));; return 500; }
+
+
+
+// It's become pretty annoying that SWS separates out http from https so severely.
+// This is nearly an exact clone of the above class, for http.
+
+class HttpClient : public Client<HTTP>
+{
+  typedef Client<HTTP> inherited;
+
+public:
+  HttpClient(const string& url)
+    :
+  // Call base class
+    inherited::Client(url)
+  {}
+
+  std::shared_ptr<Response> make_request(
+    const std::string& request_type,
+    const std::string& path="/",
+    boost::string_ref content="",
+    const std::map<std::string, std::string>& header=std::map<std::string, std::string>()
+  ) {
+      try {
+          return inherited::request(
+            request_type,     // verb
+            path,             // path
+            content,          // content
+            header            // header
+          );
+      } catch(const std::exception& e) {
+
+          // SWS throws exceptions, which is not helpful.
+          // A bad call is a bad call and needs immediate handling.
+
+          // TODO SWS makes status_ccode private so we can't set it here.
+          // Determine a good internal way to return bad status due to exception.
+          // std::shared_ptr<Response> response(new Response);
+          // response->status_code = "503 Not Availalbe -- HttpsClient exception caught";
+
+          return std::shared_ptr<Response>();
+      }
+  }
+
+  boost::asio::io_service& get_io_service() { return io_service; }
+
+};
+
+// Results extraction helpers
+static string get_body(std::shared_ptr<Client<HTTP>::Response> response)           { if (response) return string(istreambuf_iterator<char>(response->content), {}); return ""; }
+static int32_t get_status_code(std::shared_ptr<Client<HTTP>::Response> response)   { if (response) return boost::lexical_cast<int32_t>(response->status_code.substr(0,3));; return 500; }
+
 
--- c++/http/HttpsClientUtilities.h
+++ c++/http/HttpsClientUtilities.h
@@ -12,19 +12,38 @@ using namespace std;
 // ------------------------------------------------------------------------------
 
 
-static bool scrape_url(const string& url, const string& regex, string& result)
-{
-    boost::match_results<std::string::const_iterator> mresults;
-    boost::regex reg;
-    reg.assign(regex);
-    
-    // We want regex_search, not regex_match (which must match the entire input).
-    bool bSuccess = boost::regex_search(str,mresults,reg,boost::match_default);
+// INPUT:
+//  regex   provide a regex search string that includes a group; if a match is found, the group will be returned
+//          eg, for the rows in a particular table: "<table[^>]+class[^>]+width=\"100[^>]+>([\\s\\S]+?)<[/]table>"
+static bool scrape_http(
+    const string& domain,
+    const string& path,
+    const string& regex,
+    string& result
+) {
+  HttpClient client(domain);
+  auto http_response = client.make_request(
+    "GET",
+    path
+  );
 
-    if (bSuccess && what.size() > 1)
-      result = string(what[1].first,what[1].second);
-      
-    return bSuccess;
+  if (get_status_code(http_response) != 200)
+    return false;
+
+  string body = get_body(http_response);
+
+  boost::match_results<std::string::const_iterator> mresults;
+  boost::regex reg;
+  reg.assign(regex);
+
+  // We want regex_search, not regex_match (which must match the entire input).
+  bool bSuccess = boost::regex_search(body,mresults,reg,boost::match_default);
+
+  if (bSuccess && mresults.size() > 1)
+    result = string(mresults[1].first,mresults[1].second);
+
+  return bSuccess;
 
 }
+
 
