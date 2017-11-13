ab53301bd113c09fba29b636e3b5531a7fa0e89b
Michael Behrns-Miller [viper-ubuntu] <m@thedigitalmachine.com>
2017-11-12 22:23:15 -0500

    all

--- c++/scrap/portable/scrap.cpp
+++ c++/scrap/portable/scrap.cpp
@@ -745,7 +745,10 @@ int main(int argc, char *argv[])
     job_stage = 3;
 
     // Let's see what happens if we don't join until after the thread is done.
-    std::this_thread::sleep_for(300ms);
+
+
+
+
     cout << "done sleeping" << endl;
 
     // NOW we will block to ensure the thread finishes.
