ec452ed73ea5c10a84a965d8f3fda6334cdf0af2
Michael Behrns-Miller [viper] <m@thedigitalmachine.com>
2018-03-07 15:28:50 -0500

    auto-sync

--- JavaScript/scrap/fizzbuzz.js
+++ JavaScript/scrap/fizzbuzz.js
@@ -1,5 +1,22 @@
 #!/usr/bin/env node
 
+// find the longest palindrome
+// in an input string
+
+var find_pal = function(in)
+{
+  int bestlen = 0
+  int beststart = -1
+  function is_pal(in,startchar)
+  {
+
+  }
+  for (var startchar = 0; startchar < in.length(); startchar++)
+  {
+    if (is_pal(in.substring(startchar)))
+  }
+}
+
 var count = 100;
 for (var i=0; i<100; i++)
 {
