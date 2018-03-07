86293b32066845ba96640c0f5c0905b754b92649
Michael Behrns-Miller [viper] <m@thedigitalmachine.com>
2018-03-07 09:23:24 -0500

    Javascript closures scrap

--- JavaScript/scrap/closures.js
+++ JavaScript/scrap/closures.js
@@ -19,3 +19,25 @@ var globalVar = "xyz";
 
   })(456);
 })(123);
+
+
+var cascade = function() {
+
+  // Sorry charlie, the loop will complete before any timeout fires, you'll get all 5's
+  for (var i = 0; i < 5; i++) {
+    setTimeout(function() { console.log(i); }, i * 1000 );
+  }
+
+  // USE let to cause i to have "block scope" (ie it will NOT be shared with internal scope!)
+  for (let i = 0; i < 5; i++) {
+    setTimeout(function() { console.log(i); }, i * 1000 );
+  }
+
+  // Or explicitly use a closure
+  for (var i = 0; i < 5; i++) {
+    (function(x) {
+      setTimeout(function() { console.log(x); }, x * 1000 );
+    })(i);
+  }
+}
+cascade()
