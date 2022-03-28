ff3847031b64bd0d61a33320109cf33f30e84dfc
Michael Behrns-Miller [cast] <m@thedigitalmachine.com>
2022-03-28 19:48:57 -0400

    Cast config

--- JavaScript/scrap/strings.js
+++ JavaScript/scrap/strings.js
@@ -1,15 +1,23 @@
 #!/usr/bin/env node
 
+console.log();
+console.log('Test 1: split');
 var a = "a string"
 var b = a.split(' ');
 var c = a.split(''); // on every char
 var d = c.reverse();
 var e = d.join('');  // slaps em back together, with optional separator
-
 console.log(a+';'+b+';'+c+';'+d+';'+e)
+console.log();
 
-var f = e.toUpperCase();
-console.log(f);
+console.log('\nTest 2: replace whitespace');
+const f = "a string with whitespace";
+const g = f.replace(/\W/g, '====');
+console.log(f, " => ", g);
+console.log();
 
-var g = f.replace(/\W/g, '====');
-console.log(g);
+console.log('Test 3: replace single quotes');
+const quotedString = `'this is quoted'`;
+const unquotedString = quotedString.replace(/\'/g, '');
+console.log(quotedString, " => ", unquotedString);
+console.log();
