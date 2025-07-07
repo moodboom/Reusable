78e5675c331df384f349bb5663616b0a8404f0cc
Michael Behrns-Miller [abtdev1] <m@thedigitalmachine.com>
2025-07-06 20:27:51 -0400

    Use C++21 std::chrono and std::format

--- c++/basic_types.hpp
+++ c++/basic_types.hpp
@@ -1,10 +1,8 @@
 #ifndef basic_types_hpp
 #define basic_types_hpp
 
-
 // I think this has to come before STL?
 #include <boost/config.hpp>
-
 
 // ===========================================
 // All the STL support we need
@@ -15,15 +13,14 @@
 #include <iostream>
 #include <iterator>
 #include <sstream>
-#include <unordered_map>		// for C++11 unordered_map and unordered_multimap
+#include <unordered_map> // C++11 unordered_map and unordered_multimap
 #include <unordered_set>
-// #include <format>				// C++20, not implemented by any compiler as of 2020-11
+#include <chrono> // C++20; for me, arrived in Ubuntu 25.04
+#include <format> // C++20; arrived ~2023 i guess?
 // ===========================================
-
 
 // My STL container extensions (ie sorted_vector)
 #include "STLContainers.h"
-
 
 // ===========================================
 // Basic boost support
@@ -42,15 +39,14 @@
 #include <boost/crc.hpp>
 
 #include <boost/date_time/posix_time/posix_time.hpp>
-using namespace boost::posix_time;    // for ptime
-using namespace boost::gregorian;     // for date
+using namespace boost::posix_time; // for ptime
+using namespace boost::gregorian;	 // for date
 
 // ranged for loops with an index:
 //      for (const auto& element : boost::adaptors::index(my_container))
 //          cout << element.value() << element.index();
 #include <boost/range/adaptor/indexed.hpp>
 // ===========================================
-
 
 // ===========================================
 // 32 vs 64 bit ENVIRONMENT CHECK
@@ -74,26 +70,25 @@ using namespace boost::gregorian;     // for date
 #endif
 // ===========================================
 
-
 // ===========================================
 // cross-platform threading and i/o
 // ===========================================
 #ifdef WIN32
 
-	// we need to define the target system
-	#define _WIN32_WINNT 0x0501
+// we need to define the target system
+#define _WIN32_WINNT 0x0501
 
-	// we also need a compiled lib:
-	//	libboost_system-vc100-mt-1_53.lib
-	//
-	// to get it, first build boost:
-	//
-	//		cd boost
-	//		bootstrap.bat
-	//		.\b2
-	//
-	// then add the lib path to the project:
-	//	C:\Software Development\boost_1_53_0\stage\lib
+// we also need a compiled lib:
+//	libboost_system-vc100-mt-1_53.lib
+//
+// to get it, first build boost:
+//
+//		cd boost
+//		bootstrap.bat
+//		.\b2
+//
+// then add the lib path to the project:
+//	C:\Software Development\boost_1_53_0\stage\lib
 
 #endif
 #include <boost/asio.hpp>
@@ -101,7 +96,6 @@ using namespace boost::gregorian;     // for date
 #include <boost/chrono.hpp>
 #include <boost/date_time.hpp>
 // ===========================================
-
 
 // ===========================================
 // https (taken from boost docs)
@@ -111,10 +105,9 @@ using boost::asio::ip::tcp;
 namespace ssl = boost::asio::ssl;
 // NOTE that we use the form that requires an existing socket.
 // See: http://www.boost.org/doc/libs/1_57_0/doc/html/boost_asio/overview/ssl.html
-typedef ssl::stream<tcp::socket&> ssl_socket;
+typedef ssl::stream<tcp::socket &> ssl_socket;
 typedef ssl::stream<tcp::socket> ssl_embedded_socket;
 // ===========================================
-
 
 // ===========================================
 // UUIDs
@@ -122,7 +115,7 @@ typedef ssl::stream<tcp::socket> ssl_embedded_socket;
 // When using this in windows...
 // You need to define following macro under Project Properties--> yourapp Properties--> ALL CONFIGS -> C/C++-->Command Connector-->Additional Options
 //
-//		-D_SCL_SECURE_NO_WARNINGS 
+//		-D_SCL_SECURE_NO_WARNINGS
 //
 // so VS doesn't spew "checked iterators" warnings.
 // ===========================================
@@ -130,11 +123,10 @@ typedef ssl::stream<tcp::socket> ssl_embedded_socket;
 #define BOOST_PENDING_INTEGER_LOG2_HPP
 #include <boost/integer/integer_log2.hpp>
 // ===========================================
-#include <boost/uuid/uuid.hpp>            	// uuid class
-#include <boost/uuid/random_generator.hpp>	// uuid generators
-#include <boost/uuid/uuid_io.hpp>         	// streaming operators etc.
+#include <boost/uuid/uuid.hpp>						 // uuid class
+#include <boost/uuid/random_generator.hpp> // uuid generators
+#include <boost/uuid/uuid_io.hpp>					 // streaming operators etc.
 // ===========================================
-
 
 // ===========================================
 // graph library (BGL) support
@@ -144,7 +136,6 @@ typedef ssl::stream<tcp::socket> ssl_embedded_socket;
 #include <boost/graph/undirected_dfs.hpp>
 #include <boost/graph/graphviz.hpp>
 // ===========================================
-
 
 // ===========================================
 // writing to and reading from files
@@ -156,10 +147,8 @@ typedef ssl::stream<tcp::socket> ssl_embedded_socket;
 #include <boost/filesystem/fstream.hpp>
 // ===========================================
 
-
 using namespace std;
 using namespace boost;
-
 
 // ===========================================
 // COMMON CUSTOM TYPES
@@ -181,15 +170,14 @@ typedef std::unordered_multimap<std::string, int> hashmmap;
 //	assert(http_action_count == 5);
 typedef enum
 {
-	ha_get,				// for RETRIEVE
-	ha_post,			// for CREATE (when using server-generated ID)
-	ha_put,				// for UPDATE (also for create when client provides ID)
-	ha_patch,			// for UPDATE of a subset of fields (PUT technically requires all)
-	ha_delete,			// for DELETE
+	ha_get,		 // for RETRIEVE
+	ha_post,	 // for CREATE (when using server-generated ID)
+	ha_put,		 // for UPDATE (also for create when client provides ID)
+	ha_patch,	 // for UPDATE of a subset of fields (PUT technically requires all)
+	ha_delete, // for DELETE
 
 	http_action_count
 } http_action;
-
 
 // NOTE that for many of our JSON objects, we don't need to permanently store all the data that is extracted.
 // Strategy:
@@ -200,12 +188,11 @@ typedef enum
 class JSONableObject
 {
 public:
-    virtual ~JSONableObject() {}
+	virtual ~JSONableObject() {}
 
-    virtual bool from_JSON(const string& in) = 0;
-    virtual string to_JSON() const = 0;
-    virtual void log() const {}
+	virtual bool from_JSON(const string &in) = 0;
+	virtual string to_JSON() const = 0;
+	virtual void log() const {}
 };
-
 
 #endif
