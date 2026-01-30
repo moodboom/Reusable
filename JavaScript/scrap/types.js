d561ea618e4db6c34011f0cc8e783d66692e6565
Michael Behrns-Miller [cast] <m@thedigitalmachine.com>
2026-01-29 20:57:57 -0500

    Some js map scrap

--- JavaScript/scrap/types.js
+++ JavaScript/scrap/types.js
@@ -12,3 +12,19 @@ console.log("Is a function an object? "+(typeof myfunky == "object" ? "YES":"NO"
 console.log("It is of type function, right? "+(typeof myfunky == "function" ? "YES":"NO"));
 console.log("Checking defined var   with ('undefined' !== typeof a_var) "+('undefined' !== typeof myvar ? "YES":"NO"));
 console.log("Checking undefined var with ('undefined' !== typeof a_var) "+('undefined' !== typeof novar ? "YES":"NO"));
+
+
+// Javascript Maps
+// you can use {}, [], or new Map
+const key = 'prod-2';
+const prodHostMap = {
+  'prod-1': { dbhost:'host-1', db: 'my-prod' },
+  'prod-2': { dbhost:'host-8', db: 'my-empty' }, // es-prod
+};
+const prodDockerHost = 'prod-1';
+const proddbhost = prodHostMap[prodDockerHost].dbhost;
+console.log( prodDockerHost );
+console.log( proddbhost );
+
+// 'bad dates'
+console.log( prodHostMap['bogus']?.dbhost );
